35975dea556c5ab4a62f99381391f998ceb78623
Miguel Malabayabas <mk_mala@Miguels-MacBook-Air.local>
2026-01-05 21:24:06 -0600

    Adding new pages to the app

---
+++ pages/home.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("Beckett’s Player Development Lab")
---
+++ pages/the_turf.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+st.title("The Turf: Powered by D1")
+st.write("Podcast Description:")
+
+st.write("The Turf: Powered By D1 is the podcast where sports, performance, and culture collide. Hosted by D1 Training founder/CEO Will Bartholomew, each episode brings listeners onto the turf of D1 facilities for conversations with athletes, coaches, celebrities and influencers to talk all things training, coaching and more. Guests range from former pros and Olympians to current coaches and entertainers, all sharing stories of growth, resilience, and what it takes to succeed in and out of competition.")
+st.write("The Turf isn’t just about training—it’s about the mindset behind it. Listeners get a front-row seat to discussions on building confidence, navigating challenges, fueling peak performance, and balancing the demands of sport with life beyond the field. With inspiring perspectives and practical takeaways, The Turf connects the dots between athletic development, personal growth, and community.")
+st.write("Whether you’re a parent, coach, or competitor, The Turf offers insight and motivation for anyone chasing excellence.")
+
+with st.expander("**Episode 1: Vanderbilt Football Coach Clark Lea on Building Culture and Training Athletes**"):
+    
+    st.video(data="https://www.youtube.com/watch?v=KpAJBfqVYCA&list=PL4co_EcEZgmpVUz_WhDqgVjsWlt0kMXrN&index=6")
+    
+    st.write("Vanderbilt head coach Clark Lea sits down with The Turf: Powered by D1 to reflect on his journey from athlete to coach and the mentors who shaped him along the way. He shares how struggles in baseball led him back to football, the lessons he carried from coaching giants like Rick Neuheisel and Brian Kelly, and why accountability, discipline, and growth are the foundation of his program. Lea also offers insight into parenting young athletes and what he envisions for the future of Vanderbilt football.")
+
+with st.expander("**Episode 2: Golden Tate on Multi-Sport Training, NFL Mindset & Life After Football**"):
+    
+    st.video(data="https://www.youtube.com/watch?v=ujm0XW50mNo&list=PL4co_EcEZgmpVUz_WhDqgVjsWlt0kMXrN&index=5")
+    
+    st.write("Golden Tate sits down with The Turf: Powered by D1 to talk about his path from Nashville to the NFL, the value of multi-sport training, and the mindset that kept him at the top. He also shares lessons on resilience, coaching, and being intentional on and off the field.")
+
+with st.expander("**Episode 3: R.A. Dickey: Cy Young Winner on Knuckleballs, Training & Fatherhood**"):
+    
+    st.video(data="https://www.youtube.com/watch?v=pOlfdzsJQSw&list=PL4co_EcEZgmpVUz_WhDqgVjsWlt0kMXrN&index=4")
+
+    st.write("In this episode of The Turf: Powered by D1, host Will Bartholomew sits down with MLB legend and Cy Young winner R.A. Dickey. Together, they dive into R.A.'s transformation from power pitcher to knuckleball master, the training and mindset shifts that fueled his late-career success, and the lessons he’s carrying into fatherhood and coaching today.")
+    st.write("From behind-the-scenes stories in the big leagues to honest reflections on raising kids in today’s sports culture, R.A. shares wisdom, humor, and perspective that every athlete and parent can learn from.")
+
+with st.expander("**Episode 4: Luke Bryan on Sports, Parenting, and the Drive to Stay in Shape**"):
+    
+    st.video(data="https://www.youtube.com/watch?v=4feFbBLJcVg&list=PL4co_EcEZgmpVUz_WhDqgVjsWlt0kMXrN&index=3")
+
+    st.write("Country superstar Luke Bryan joins The Turf: Powered by D1 to talk about the lessons sports taught him, how he’s raising his boys through athletics, and why staying active still drives him today. From throwing a no-hitter in high school to coaching youth football with bubblegum bribes, Luke shares hilarious stories, heartfelt parenting moments, and insights on faith, fitness, and fatherhood.")
+    st.write("Hear how he balances touring, American Idol, and family life — and what Tim McGraw, Chipper Jones, and Peyton Manning have all taught him about discipline and leadership.")
+
+with st.expander("**Episode 5: Chase Rice on Football, Music, and Finding Purpose Beyond the Game**"):
+    
+    st.video(data="https://www.youtube.com/watch?v=c6jooDF2iU0&list=PL4co_EcEZgmpVUz_WhDqgVjsWlt0kMXrN&index=2")
+
+    st.write("Chase Rice was built for the game — a 195-pound linebacker at UNC, fast, fearless, and ready for the next level. Then, one play changed everything.")
+    st.write("What followed was a journey from football fields to NASCAR pit crews to the country music stage — and the lessons that came with it.")
+    st.write("In this episode of The Turf: Powered by D1, Chase and host Will Bartholomew talk toughness, leadership, mentorship, loss, and what it really means to reinvent yourself when the dream you built your life around disappears.")
+
+st.write("Source: www.d1training.com")
